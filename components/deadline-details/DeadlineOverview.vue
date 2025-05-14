1bd98b32adcc1c7f65ff2512079612b232142842
charles <barsubiacharles@gmail.com>
2025-05-15 03:11:18 +0800

    Minor fix on select dropdown appearance.

--- components/deadline-details/DeadlineOverview.vue
+++ components/deadline-details/DeadlineOverview.vue
@@ -235,7 +235,7 @@ watch(deadline, () => {
                 <select
                   v-if="editMode"
                   v-model="priority"
-                  class="mt-1 block w-full rounded-md border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm dark:text-white"
+                  class="mt-1 block w-full rounded-md appearance-none border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm dark:text-white"
                 >
                   <option :value="1">Lowest</option>
                   <option :value="2">Low</option>
@@ -319,7 +319,7 @@ watch(deadline, () => {
                 <select
                   v-if="editMode"
                   v-model="assignedTo"
-                  class="mt-1 block w-full rounded-md border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm dark:text-white"
+                  class="mt-1 block w-full rounded-md appearance-none border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm dark:text-white"
                 >
                   <option v-for="user in users" :key="user.id" :value="user.id">
                     {{ user.fullname }}
